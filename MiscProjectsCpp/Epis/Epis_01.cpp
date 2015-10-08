75093d16496d88b775bf76f27186f50c5ff545d1
Michael L. Brock <michael.brock@gmail.com>
2015-10-08 14:58:18 -0400

    WIP

--- MiscProjectsCpp/Epis/Epis_01.cpp
+++ MiscProjectsCpp/Epis/Epis_01.cpp
@@ -28,6 +28,15 @@
 //	Required include files...
 // ////////////////////////////////////////////////////////////////////////////
 
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <set>
+#include <vector>
+
+#include <algorithm>
+#include <numeric>
+
 //	////////////////////////////////////////////////////////////////////////////
 
 //	////////////////////////////////////////////////////////////////////////////
@@ -36,9 +45,11 @@ typedef unsigned int ObservationType;
 
 //	////////////////////////////////////////////////////////////////////////////
 class Epis_00 {
+	typedef std::multiset<ObservationType> MySet;
+	typedef MySet::const_iterator          MySetIterC;
 public:
 	Epis_00()
-		:observations_()
+		:obs_set_()
 	{
 	}
 
@@ -47,14 +58,37 @@ public:
 		obs_set_.insert(obs);
 	}
 
-	std::multiset<ObservationType> obs_set_;
+	ObservationType GetValueForPercentile(ObservationType percentile) const
+	{
+		if (obs_set_.empty())
+			return(0);
+
+		MySetIterC iter_b(obs_set_.begin());
+		MySetIterC iter_e(obs_set_.end());
+
+		ObservationType last_value = 0;
+
+		for (std::size_t count_1 = 1; iter_b != iter_e; ++iter_b, ++count_1) {
+			double this_percentile = 100.0 *
+				(static_cast<double>(count_1) - .5) /
+				static_cast<double>(obs_set_.size());
+			if (this_percentile > static_cast<double>(percentile))
+				break;
+			last_value = *iter_b;
+		}
+
+		return(last_value);
+	}
+
+private:
+	MySet obs_set_;
 };
 //	////////////////////////////////////////////////////////////////////////////
 
 //	////////////////////////////////////////////////////////////////////////////
 struct DatumPoint {
 	DatumPoint(ObservationType obs = 0, std::size_t occurrences = 0)
-		:value_(value)
+		:value_(obs)
 		,occurrences_(occurrences)
 	{
 	}
@@ -72,6 +106,7 @@ struct DatumPoint {
 //	////////////////////////////////////////////////////////////////////////////
 typedef std::set<DatumPoint>                DatumPointSet;
 typedef DatumPointSet::const_iterator       DatumPointSetIterC;
+typedef DatumPointSet::const_iterator       DatumPointSetIterC;
 typedef std::pair<DatumPointSetIterC, bool> DatumPointSetInsResult;
 //	////////////////////////////////////////////////////////////////////////////
 
@@ -79,7 +114,7 @@ typedef std::pair<DatumPointSetIterC, bool> DatumPointSetInsResult;
 class Epis_01 {
 public:
 	Epis_01()
-		:observations_()
+		:obs_set_()
 	{
 	}
 
@@ -88,12 +123,245 @@ public:
 		DatumPointSetInsResult ins_result(obs_set_.insert(DatumPoint(obs, 1)));
 
 		if (!ins_result.second)
-			const_cast<DatumPoint *>(&(*ins_result.first))->value_ = obs;
+			const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_++;
 	}
 
-	DatumPointSet observations_;
+private:
+	DatumPointSet obs_set_;
 };
 //	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+struct DatumPointItem {
+	DatumPointItem()
+		:base_count_(0)
+		,obs_count_(0)
+		,obs_set_()
+	{
+	}
+
+	unsigned int  base_count_;
+	unsigned int  obs_count_;
+	DatumPointSet obs_set_;
+};
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+typedef std::vector<DatumPointItem>          DatumPointItemVector;
+typedef DatumPointItemVector::const_iterator DatumPointItemVectorIterC;
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+class Epis_02 {
+public:
+	Epis_02()
+		:obs_set_()
+		,obs_set_total_(0)
+		,obs_vector_()
+	{
+	}
+
+	void AddObservation(ObservationType obs)
+	{
+		if (obs_vector_.empty()) {
+			AddObservationPrep(obs);
+			if (obs_set_.size() < 100)
+				return;
+			//	Populate;
+			obs_vector_.resize(100);
+			unsigned int       interval_size = obs_set_total_ / 100;
+			std::size_t        current_size  = 0;
+			std::size_t        current_index = 0;
+			DatumPointSetIterC iter_b(obs_set_.begin());
+			DatumPointSetIterC iter_e(obs_set_.end());
+			for ( ; iter_b != iter_e; ) {
+				if ((current_size + iter_b->occurrences_) <= interval_size) {
+					current_size += iter_b->occurrences_;
+					obs_vector_[current_index].obs_set_.insert(*iter_b);
+					const_cast<DatumPoint *>(&(*iter_b))->occurrences_ = 0;
+					obs_vector_[current_index].base_count_ += iter_b->occurrences_;
+					obs_vector_[current_index].obs_count_  += iter_b->occurrences_;
+					if (current_size == interval_size)
+						++current_index;
+					++iter_b;
+				}
+				else {
+					//	Split the observation...
+					DatumPoint tmp_point(*iter_b);
+					tmp_point.occurrences_ -= current_size;
+					obs_vector_[current_index].obs_set_.insert(tmp_point);
+					obs_vector_[current_index].base_count_ += tmp_point.occurrences_;
+					obs_vector_[current_index].obs_count_  += tmp_point.occurrences_;
+					const_cast<DatumPoint *>(&(*iter_b))->occurrences_ -=
+						(interval_size - current_size);
+					++current_index;
+				}
+			}
+
+			--iter_b;
+			if (iter_b->occurrences_) {
+				DatumPointSetIterC iter_f(
+					obs_vector_[99].obs_set_.find(*iter_b));
+				if (iter_f == obs_vector_[99].obs_set_.end())
+					obs_vector_[99].obs_set_.insert(*iter_b);
+				else
+					const_cast<DatumPoint *>(&(*iter_f))->occurrences_ +=
+						iter_b->occurrences_;
+				obs_vector_[99].base_count_ += iter_b->occurrences_;
+				obs_vector_[99].obs_count_  += iter_b->occurrences_;
+			}
+
+			obs_set_.clear();
+
+			return;
+		}
+
+		//	Obs are ordered within vec, so a bisection search is possible...
+		for (std::size_t count_1 = count_1; count_1 < 100; ++count_1) {
+			if ((obs_vector_[count_1].obs_set_.begin()->value_ <= obs) &&
+				 (obs_vector_[count_1].obs_set_.rbegin()->value_ >= obs)) {
+				DatumPointSetInsResult ins_result(
+					obs_vector_[count_1].obs_set_.insert(DatumPoint(obs, 1)));
+				if (!ins_result.second)
+					++const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_;
+				++obs_vector_[count_1].obs_count_;
+				if ((obs_vector_[count_1].obs_count_ -
+					obs_vector_[count_1].base_count_) >= 100) {
+					// Partial re-balancing...
+					if (!count_1)
+						RebalanceRight(count_1, 100);
+					else if (count_1 == 99)
+						RebalanceLeft(count_1, 100);
+					else {
+						RebalanceRight(count_1, 99 - count_1);
+						RebalanceLeft(count_1, count_1 - 1);
+					}
+				}
+			}
+		}
+	}
+#if 0
+#endif // #if 0
+
+	ObservationType GetValueForPercentile(ObservationType percentile) const
+	{
+		if ((obs_set_.size() < 100) || (percentile < 1) || (percentile > 100))
+			return(0);
+
+		return(GetElementMean(percentile));
+	}
+
+private:
+	DatumPointSet        obs_set_;
+	unsigned int         obs_set_total_;
+	DatumPointItemVector obs_vector_;
+
+	ObservationType GetElementMean(std::size_t element_index) const
+	{
+		double             working_total = 0.0;
+		DatumPointSetIterC iter_b(obs_vector_[element_index].obs_set_.begin());
+		DatumPointSetIterC iter_e(obs_vector_[element_index].obs_set_.end());
+
+		for ( ; iter_b != iter_e; ++iter_b)
+			working_total += iter_b->value_ * iter_b->occurrences_;
+
+		return(static_cast<ObservationType>(working_total /
+			static_cast<double>(obs_vector_[element_index].obs_count_)));
+	}
+
+	void AddObservationPrep(ObservationType obs)
+	{
+		DatumPointSetInsResult ins_result(obs_set_.insert(DatumPoint(obs, 1)));
+
+		if (!ins_result.second)
+			const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_++;
+
+		++obs_set_total_;
+	}
+
+	void RebalanceLeft(std::size_t src_index, std::size_t rebalance_count)
+	{
+		RebalanceHelper(src_index, rebalance_count, -1);
+	}
+
+	void RebalanceRight(std::size_t src_index, std::size_t rebalance_count)
+	{
+		RebalanceHelper(src_index, rebalance_count, 1);
+	}
+
+	void RebalanceHelper(std::size_t src_index, std::size_t rebalance_count,
+		int direction)
+	{
+		DatumPointItem &src_element(obs_vector_[src_index]);
+		DatumPointItem &dst_element(obs_vector_[(direction == -1) ?
+			(src_index - 1) : (src_index + 1)]);
+		std::size_t     remaining_size = rebalance_count;
+
+		while (remaining_size) {
+			DatumPointSetIterC src_iter((direction == -1) ?
+				src_element.obs_set_.begin() : --src_element.obs_set_.end());
+			if (src_iter->occurrences_ <= remaining_size) {
+				remaining_size -= src_iter->occurrences_;
+				DatumPointSetInsResult ins_result(
+					dst_element.obs_set_.insert(*src_iter));
+				if (!ins_result.second)
+					const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_ +=
+						src_iter->occurrences_;
+				dst_element.obs_count_ += src_iter->occurrences_;
+				src_element.obs_set_.erase(src_iter);
+			}
+			else {
+				DatumPoint tmp_point(*src_iter);
+				tmp_point.occurrences_ -= remaining_size;
+				remaining_size          = 0;
+
+				DatumPointSetInsResult ins_result(
+					dst_element.obs_set_.insert(tmp_point));
+				if (!ins_result.second)
+					const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_ +=
+						tmp_point.occurrences_;
+				dst_element.obs_count_ += tmp_point.occurrences_;
+			}
+		}
+	}
+#if 0
+#endif // #if 0
+
+};
+//	////////////////////////////////////////////////////////////////////////////
+
+/*
+//	////////////////////////////////////////////////////////////////////////////
+class Epis_03 {
+	std::size_t PercGran = 100;
+public:
+	Epis_03()
+		:obs_set_()
+	{
+	}
+
+	void AddObservation(ObservationType obs)
+	{
+		DatumPointSetInsResult ins_result(obs_set_.insert(DatumPoint(obs, 1)));
+
+		if (!ins_result.second)
+			const_cast<DatumPoint *>(&(*ins_result.first))->occurrences_++;
+	}
+
+	ObservationType GetValueForPercentile(ObservationType percentile) const
+	{
+		if ((percentile < 1) || (percentile > 100))
+			return(0);
+
+		for ()
+		return(obs_vector_[percentile])
+	}
+
+private:
+	DatumPointSet obs_set_;
+}
+//	////////////////////////////////////////////////////////////////////////////
+*/
 
 namespace {
 
@@ -103,15 +371,30 @@ const std::size_t     MyTestCount  = sizeof(MyTestList) / sizeof(MyTestList[0]);
 //	////////////////////////////////////////////////////////////////////////////
 
 //	////////////////////////////////////////////////////////////////////////////
+void TEST_RunPopulate(int value_count)
+{
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+template <typename DatumType> DatumType &TEST_BasicTest()
+{
+	DatumType datum;
+
+	for (std::size_t count_1 = 0; count_1 < MyTestCount; ++count_1)
+		datum.AddObservation(MyTestList[count_1]);
+
+	std::cout << datum.GetValueForPercentile(50) << std::endl;
+
+	return(datum);
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
 void TEST_RunMe()
 {
-	Epis_00 epis_00;
-	Epis_01 epis_01;
-
-	for (std::size_t count_1 = 0; count_1 < MyTestCount; ++count_1) {
-		epis_00.AddObservation(MyTestList[count_1]);
-		epis_01.AddObservation(MyTestList[count_1]);
-	}
+	Epis_00 epis_00(TEST_BasicTest<Epis_00>());
+	Epis_02 epis_02(TEST_BasicTest<Epis_02>());
 }
 //	////////////////////////////////////////////////////////////////////////////
 
@@ -126,7 +409,7 @@ int main(int argc, char **argv)
 		if (argc < 2)
 			TEST_RunMe();
 		else
-			RunPopulate(::atoi(argv[1]));
+			TEST_RunPopulate(::atoi(argv[1]));
 	}
 	catch (const std::exception &except) {
 		std::cerr << "ERROR: " << except.what() << std::endl;
