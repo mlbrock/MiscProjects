7e67eb2dabad851826de873802d3d0a9e0fd9f80
Michael L. Brock <michael.brock@gmail.com>
2015-10-21 08:17:46 -0400

    Clean-up

--- MiscProjectsCpp/Epis/Epis_01.cpp
+++ MiscProjectsCpp/Epis/Epis_01.cpp
@@ -25,49 +25,6 @@
 // ////////////////////////////////////////////////////////////////////////////
 /**
 	\file MiscProjects/MiscProjectsCpp/Epis_01.cpp
-
-	Discussion:
-	-----------
-	Not being a numerical methods man, I had to refresh my knowledge of
-	how to calculate the damned things.
-
-	All of code below is based upon my belief that you guys wanted me to
-	figure it out for myself. In the real world I'd have just Google'd for
-	it, and hacked the code to meet quality standards. Or possibly Numerical
-	Recipes has something I could use. So I came up with all on my very own.
-	Ugly, yes, but it does seem to work.
-
-	This work is essentially the result of exploratory code refined over the
-	(more than two) hours I've worked on it. I figure there is some cool and
-	elegant way to do this known to all discrete math types. Which I'll try
-	to Google-up after signing-off on this effort. Or if you'd like to point
-	me at a resource, feel free.
-
-	Some Implementation Notes:
-	---- -------------- ------
-	Epis_00: is a quick and simple implementation which gave me an in-code
-	mechanism for checking my work. Checked first against the web page
-	mentioned above.
-
-	Epis_01: was my first cut at a solution: Not very smart, but simple
-	enough to rely upon.
-
-	Epis_02: is what I'm turning in for the grade. It uses Epis_01 to
-	jump-start its internal data through what seems upon reflection a
-	much too-convolute approach. Another overly convolute mechanism is
-	the re-balancing. I must confess I've not given it the hammering in
-	test it deserves, but spot-checking it with constructed inputs
-	shows it works (for those inputs at least). It is, however, _extremely_
-	sensitive to the order in which observations are presented. For an
-	example of this sensitivity, try running it once with no command line
-	parameters, and a second time with the single parameter "noshuffle".
-
-	Development Info:
-	----------- -----
-	gcc --version  --->	gcc (GCC) 4.8.2 20131212 (Red Hat 4.8.2-7)
-	uname -a			--->  Linux localhost.localdomain 3.12.8-300.fc20.x86_64 #1 SMP Thu Jan 16 01:07:50 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux
-
-	But should be pretty portable; I just compiled and ran it under Windows.
 */
 // ////////////////////////////////////////////////////////////////////////////
 
@@ -529,10 +486,6 @@ std::size_t     MyTestCount_1  =
 //	////////////////////////////////////////////////////////////////////////////
 std::size_t     MyTestCount_2     = 200;
 ObservationType MyTestList_2[200] = {1, 3, 5, 7, 9, 9, 14, 14, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211};
-/*
-ObservationType MyTestIdx_2[200]  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200};
-double          MyTestPerc_2[200] = {0.25, 0.75, 1.25, 1.75, 2.25, 2.75, 3.25, 3.75, 4.25, 4.75, 5.25, 5.75, 6.25, 6.75, 7.25, 7.75, 8.25, 8.75, 9.25, 9.75, 10.25, 10.75, 11.25, 11.75, 12.25, 12.75, 13.25, 13.75, 14.25, 14.75, 15.25, 15.75, 16.25, 16.75, 17.25, 17.75, 18.25, 18.75, 19.25, 19.75, 20.25, 20.75, 21.25, 21.75, 22.25, 22.75, 23.25, 23.75, 24.25, 24.75, 25.25, 25.75, 26.25, 26.75, 27.25, 27.75, 28.25, 28.75, 29.25, 29.75, 30.25, 30.75, 31.25, 31.75, 32.25, 32.75, 33.25, 33.75, 34.25, 34.75, 35.25, 35.75, 36.25, 36.75, 37.25, 37.75, 38.25, 38.75, 39.25, 39.75, 40.25, 40.75, 41.25, 41.75, 42.25, 42.75, 43.25, 43.75, 44.25, 44.75, 45.25, 45.75, 46.25, 46.75, 47.25, 47.75, 48.25, 48.75, 49.25, 49.75, 50.25, 50.75, 51.25, 51.75, 52.25, 52.75, 53.25, 53.75, 54.25, 54.75, 55.25, 55.75, 56.25, 56.75, 57.25, 57.75, 58.25, 58.75, 59.25, 59.75, 60.25, 60.75, 61.25, 61.75, 62.25, 62.75, 63.25, 63.75, 64.25, 64.75, 65.25, 65.75, 66.25, 66.75, 67.25, 67.75, 68.25, 68.75, 69.25, 69.75, 70.25, 70.75, 71.25, 71.75, 72.25, 72.75, 73.25, 73.75, 74.25, 74.75, 75.25, 75.75, 76.25, 76.75, 77.25, 77.75, 78.25, 78.75, 79.25, 79.75, 80.25, 80.75, 81.25, 81.75, 82.25, 82.75, 83.25, 83.75, 84.25, 84.75, 85.25, 85.75, 86.25, 86.75, 87.25, 87.75, 88.25, 88.75, 89.25, 89.75, 90.25, 90.75, 91.25, 91.75, 92.25, 92.75, 93.25, 93.75, 94.25, 94.75, 95.25, 95.75, 96.25, 96.75, 97.25, 97.75, 98.25, 98.75, 99.25, 99.75};
-*/
 // ////////////////////////////////////////////////////////////////////////////
 
 //	////////////////////////////////////////////////////////////////////////////
