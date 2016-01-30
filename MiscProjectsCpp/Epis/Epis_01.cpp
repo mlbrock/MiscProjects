3adc360a3cb4c15897836acb2aabb71811d96b31
Michael L. Brock <michael.brock@gmail.com>
2016-01-29 23:07:36 -0500

    Change copyright year
    Change end of copyright year range from 2015 to 2016.

--- MiscProjectsCpp/Epis/Epis_01.cpp
+++ MiscProjectsCpp/Epis/Epis_01.cpp
@@ -14,7 +14,7 @@
 								Ian Robertson, January 09, 2004
 								http://web.stanford.edu/class/archive/anthsci/anthsci192/anthsci192.1064/handouts/calculating%20percentiles.pdf
 
-		Copyright Michael L. Brock 2015 - 2015.
+		Copyright Michael L. Brock 2015 - 2016.
 		Distributed under the Boost Software License, Version 1.0.
 		(See accompanying file LICENSE_1_0.txt or copy at
 		http://www.boost.org/LICENSE_1_0.txt)
