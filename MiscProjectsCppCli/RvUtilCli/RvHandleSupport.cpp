17a21bd7883fd1ea7955fbb02501efb65ad08e27
mlbrock <MichaelLBrock@yahoo.com>
2018-04-23 14:59:44 -0400

    Fix start of copyright year range

--- MiscProjectsCppCli/RvUtilCli/RvHandleSupport.cpp
+++ MiscProjectsCppCli/RvUtilCli/RvHandleSupport.cpp
@@ -9,7 +9,7 @@
 	Revision History	:	2005-02-28 --- Creation.
 									Michael L. Brock
 
-		Copyright Michael L. Brock 20o5 - 2016.
+		Copyright Michael L. Brock 2005 - 2016.
 
 		Distributed under the Boost Software License, Version 1.0.
 		(See accompanying file LICENSE_1_0.txt or copy at
