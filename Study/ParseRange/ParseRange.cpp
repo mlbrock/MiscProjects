2cbc0fb961632040b9291b94ed8d72d532c7b265
mlbrock <MichaelLBrock@yahoo.com>
2018-07-26 06:44:01 -0400

    WIP

--- Study/ParseRange/ParseRange.cpp
+++ Study/ParseRange/ParseRange.cpp
@@ -15,9 +15,10 @@
 
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/algorithm/string/split.hpp>
-//#include <boost/algorithm/string/replace.hpp>
 
 #include <boost/regex/v4/regex.hpp>
+
+#include <boost/bimap.hpp>
 
 #include <typeinfo>
 
@@ -43,6 +44,69 @@ template <typename DatumType>
 		DatumType max_value = std::numeric_limits<DatumType>::max())
 {
 	return(ParseNumericString<DatumType>(src.c_str(), min_value, max_value));
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+//	////////////////////////////////////////////////////////////////////////////
+template <typename DatumType>
+	std::ostream & operator << (std::ostream &o_str,
+		const std::pair<DatumType, DatumType> &datum)
+{
+	o_str <<
+		'[' << std::setw(20) << datum.first  <<
+		',' << std::setw(20) << datum.second << ']';
+
+	return(o_str);
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+template <typename DatumType>
+	std::ostream & ToStream(std::ostream &o_str,
+		const std::vector<std::pair<DatumType, DatumType> > &datum,
+		const std::string &prefix = "")
+{
+	for (std::size_t count_1 = 0; count_1 < datum.size(); ++count_1)
+		o_str << ((count_1) ? "\n" : "") << prefix << datum[count_1];
+
+	return(o_str);
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+template <typename DatumType>
+	std::ostream & ToStream(std::ostream &o_str,
+		const std::map<DatumType, DatumType> &datum,
+		const std::string &prefix = "")
+{
+	typename std::map<DatumType, DatumType>::const_iterator iter_b(datum.begin());
+//	std::map<DatumType, DatumType>::const_iterator iter_e(datum.end());
+
+	for (std::size_t count_1 = 0; count_1 < datum.size(); ++count_1, ++iter_b)
+		o_str << ((count_1) ? "\n" : "") << prefix <<
+			std::pair<DatumType, DatumType>(iter_b->first, iter_b->second);
+
+	return(o_str);
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+template <typename DatumType>
+	std::ostream & operator << (std::ostream &o_str,
+		const std::vector<std::pair<DatumType, DatumType> > &datum)
+{
+/*
+	for (std::size_t count_1 = 0; count_1 < datum.size(); ++count_1)
+		o_str << ((count_1) ? "\n" : "") << prefix << datum[count_1];
+
+	return(o_str);
+*/
+	return(ToStream(o_str, datum));
 }
 //	////////////////////////////////////////////////////////////////////////////
 
@@ -92,9 +156,15 @@ template <typename DatumType>
 //	////////////////////////////////////////////////////////////////////////////
 template <typename DatumType> struct IntervalWrapper {
 //	typedef DatumType                           DatumType;
-	typedef std::pair<DatumType, DatumType>     ValInterval;
-	typedef std::vector<ValInterval>            ValIntervalVec;
-	typedef std::multimap<DatumType, DatumType> ValIntervalMap;
+	typedef std::pair<DatumType, DatumType>         ValInterval;
+	typedef std::vector<ValInterval>                ValIntervalVec;
+	typedef std::map<DatumType, DatumType>          ValIntervalMap;
+	typedef typename ValIntervalMap::iterator       ValIntervalMapIter;
+	typedef typename ValIntervalMap::const_iterator ValIntervalMapIterC;
+
+	typedef boost::bimap<DatumType, DatumType>        ValIntervalBiMap;
+	typedef typename ValIntervalBiMap::iterator       ValIntervalBiMapIter;
+	typedef typename ValIntervalBiMap::const_iterator ValIntervalBiMapIterC;
 
 	static ValIntervalVec ParseInterval(const std::string &src,
 		DatumType min_value = std::numeric_limits<DatumType>::min(),
@@ -110,6 +180,7 @@ template <typename DatumType> struct IntervalWrapper {
 		std::string              tmp_src(src);
 		std::vector<std::string> tmp_list;
 		ValIntervalVec           range_list;
+ValIntervalMap           range_map;
 
 		split(tmp_list, tmp_src, is_any_of(","), token_compress_off);
 
@@ -160,6 +231,9 @@ template <typename DatumType> struct IntervalWrapper {
 				else
 					throw std::invalid_argument("Multiple interval element "
 						"separator characters were encountered.");
+
+RangeMapInsert(range_map, val_lo, val_hi);
+
 				range_list.push_back(ValInterval(val_lo, val_hi));
 			}
 			catch (const std::exception &except) {
@@ -190,8 +264,112 @@ template <typename DatumType> struct IntervalWrapper {
 			}
 		}
 
+/*
 		return(dst);
+*/
+return(ValIntervalVec(range_map.begin(), range_map.end()));
 	}
+
+private:
+
+	static ValIntervalMap &RangeMapInsert(ValIntervalMap &range_map,
+		const DatumType &val_lo, const DatumType &val_hi)
+	{
+		if (range_map.empty()) {
+			range_map.insert(ValInterval(val_lo, val_hi));
+			return(range_map);
+		}
+
+		ValIntervalMapIter iter_b(range_map.begin());
+		ValIntervalMapIter iter_e(range_map.end());
+
+		for ( ; iter_b != iter_e ; ++iter_b) {
+/*
+			if (ignore_ptr == &(*iter_b))
+				continue;
+*/
+			if ((val_lo <= iter_b->second) &&
+				 (val_hi >= iter_b->first)) {
+				//	CASES: #3, #4, #5, #6, #7, #8, #9
+				if (val_lo == iter_b->first)
+					iter_b->second = std::max(iter_b->second, val_hi);
+				else {
+					ValInterval tmp(std::min(iter_b->first, val_lo),
+						std::max(iter_b->second, val_hi));
+					range_map.erase(iter_b);
+					iter_b = range_map.insert(tmp).first;
+				}
+
+ToStream(std::cout, range_map, ">>>") << std::endl;
+
+				if (range_map.size() > 1) {
+					ValIntervalMap tmp_range_map(range_map);
+					ValInterval    tmp_interval(iter_b->first, iter_b->second);
+					tmp_range_map.erase(iter_b->first);
+					RangeMapInsert(tmp_range_map, tmp_interval.first, tmp_interval.second);
+					if (tmp_range_map != range_map)
+						range_map.swap(tmp_range_map);
+				}
+
+ToStream(std::cout, range_map, "<<<") << std::endl;
+
+				return(range_map);
+			}
+		}
+
+		range_map.insert(ValInterval(val_lo, val_hi));
+
+		return(range_map);
+	}
+
+/*
+ValIntervalBiMap range_map;
+		for (const auto &src_intv : range_list) {
+			if (range_map.empty())
+				range_map.insert(src_intv);
+			else {
+				bool merged_flag = false;
+				ValIntervalBiMapIter iter_e(range_map.end());
+				ValIntervalBiMapIter iter_tmp(range_map.find(src_intv.first));
+				if (iter_tmp != iter_e) {
+					iter_tmp->second = std::max(iter_tmp->second, src_intv.second);
+					continue;
+				}
+				ValIntervalBiMapIter iter_ub(range_map.upper_bound(src_intv.first));
+				iter_tmp = iter_ub;
+				while (iter_tmp != iter_e) {
+					if (*iter_tmp == src_intv) {
+						merged_flag = true;
+						break;
+					}
+					if ((src_intv.first <= iter_tmp->second) &&
+						 (src_intv.second >= iter_tmp->first)) {
+						ValInterval tmp_intv(
+							std::min(iter_tmp->first, src_intv.first);
+							std::max(iter_tmp->second, src_intv.second));
+						range_map.erase(iter_tmp);
+						range_map[tmp_intv.first] = tmp_intv.second;
+						merged_flag               = true;
+						break;
+					}
+					++iter_tmp;
+				}
+				for (auto &dst_intv : dst) {
+					if ((src_intv.first <= dst_intv.second) &&
+						 (src_intv.second >= dst_intv.first)) {
+						//	CASES: #3, #4, #5, #6, #7, #8, #9
+						dst_intv.first  = std::min(dst_intv.first, src_intv.first);
+						dst_intv.second = std::max(dst_intv.second, src_intv.second);
+						merged_flag     = true;
+						break;
+					}
+				}
+				if (!merged_flag)
+					dst.push_back(ValInterval(src_intv.first, src_intv.second));
+			}
+		}
+	}
+*/
 };
 //	////////////////////////////////////////////////////////////////////////////
 
@@ -217,31 +395,6 @@ template <typename DatumType> bool IsInInterval(
 	}
 
 	return(false);
-}
-//	////////////////////////////////////////////////////////////////////////////
-
-//	////////////////////////////////////////////////////////////////////////////
-template <typename DatumType>
-	std::ostream & operator << (std::ostream &o_str,
-		const std::pair<DatumType, DatumType> &datum)
-{
-	o_str <<
-		'[' << std::setw(20) << datum.first  <<
-		',' << std::setw(20) << datum.second << ']';
-
-	return(o_str);
-}
-//	////////////////////////////////////////////////////////////////////////////
-
-//	////////////////////////////////////////////////////////////////////////////
-template <typename DatumType>
-	std::ostream & operator << (std::ostream &o_str,
-		const std::vector<std::pair<DatumType, DatumType> > &datum)
-{
-	for (std::size_t count_1 = 0; count_1 < datum.size(); ++count_1)
-		o_str << ((count_1) ? "\n" : "") << datum[count_1];
-
-	return(o_str);
 }
 //	////////////////////////////////////////////////////////////////////////////
 
@@ -370,17 +523,71 @@ void TEST_RunInterval(int &return_code)
 //	////////////////////////////////////////////////////////////////////////////
 
 //	////////////////////////////////////////////////////////////////////////////
+typedef int                                       DatumType;
+typedef std::pair<DatumType, DatumType>           ValInterval;
+typedef boost::bimap<DatumType, DatumType>        ValIntervalBiMap;
+typedef typename ValIntervalBiMap::iterator       ValIntervalBiMapIter;
+typedef typename ValIntervalBiMap::const_iterator ValIntervalBiMapIterC;
+typedef typename ValIntervalBiMap::left_const_iterator ValIntervalBiMapLIterC;
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+/*
+template <typename SiMapType>
+	void TEST_RunBiMap_Helper(SiMapType &range_bimap,
+		const ValInterval &new_value)
+{
+	if (range_bimap.empty()) {
+		range_bimap.insert(new_value);
+		return;
+	}
+
+	ValIntervalBiMapIter iter_f_r(ra)
+}
+*/
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+/**/
+void TEST_RunBiMap_Helper(ValIntervalBiMap &range_bimap,
+	const ValInterval &new_value)
+{
+	if (range_bimap.empty()) {
+		range_bimap.left.insert(new_value);
+		return;
+	}
+
+//	ValIntervalBiMapLIterC iter_f_l(range_bimap.left.find(new_value.first));
+	ValIntervalBiMapLIterC iter_ub_l(range_bimap.left.upper_bound(new_value.second));
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
+void TEST_RunBiMap()
+{
+	ValIntervalBiMap range_bimap;
+
+//	range_map.insert(ValInterval(5, 10));
+	TEST_RunBiMap_Helper(range_bimap, ValInterval(5, 10));
+}
+//	////////////////////////////////////////////////////////////////////////////
+
+//	////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
 	int return_code = EXIT_SUCCESS;
 
 	try {
+//		TEST_RunBiMap();
 		TEST_RunInterval(return_code);
 	}
 	catch (const std::exception &except) {
-		return_code = EXIT_SUCCESS;
+		return_code = EXIT_FAILURE;
 		std::cerr << "\nERROR: " << except.what() << std::endl;
 	}
+
+	if (return_code = EXIT_SUCCESS)
+		std::cerr << "Regression test failed.\n" << std::endl;
 
 	return(return_code);
 }
